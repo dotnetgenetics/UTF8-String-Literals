2a2960494cffefd7545128ce36e0861769d78a1e
Gregory Nelson Esguerra <gregory_keyboard@outlook.com>
2024-02-12 18:36:08 +0800

    Add project files.

---
+++ UTF8StringLiterals/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace UTF8StringLiterals
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadOnlySpan<byte> AuthStringLiteral = "Simple Example Of UTF-8 String Literal"u8;
+            var stringLiteral = System.Text.Encoding.UTF8.GetString(AuthStringLiteral); 
+            Console.WriteLine(stringLiteral);
+
+            Console.WriteLine();
+            byte[] AnotherAuthStringLiteral = "Another Example of UTF-8 String Literal"u8.ToArray();
+            var stringAnotherLiteral = System.Text.Encoding.UTF8.GetString(AnotherAuthStringLiteral);
+            Console.WriteLine(stringAnotherLiteral);
+
+            Console.ReadLine();
+        }
+    }
+}
